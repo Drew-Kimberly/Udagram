fd915677a68a58ffdb14ee78d5bd61896f72f471
Drew Kimberly <andrew.e.kimberly@gmail.com>
2020-02-24 09:17:37 -0500

    More fixes for managing udagram-frontend in monorepo

--- scripts/travisci/monorepo-split.sh
+++ scripts/travisci/monorepo-split.sh
@@ -2,9 +2,11 @@
 
 source "../bin/split.sh"
 
+set +x
+
 # Ensure the build can push to our repos.
 mkdir -p ~/.ssh
-echo "${PACKAGE_SPLIT_SSH_KEY}" > ~/.ssh/id_rsa
+echo -n "${PACKAGE_SPLIT_SSH_KEY}" | base64 --decode > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
@@ -15,3 +17,5 @@ split_ci_job "udagram-frontend" "packages/udagram-frontend"
 split_ci_job "udagram-feed-svc" "packages/udagram-feed-svc"
 split_ci_job "udagram-user-svc" "packages/udagram-user-svc"
 split_ci_job "udagram-auth" "packages/udagram-auth"
+
+set -x
