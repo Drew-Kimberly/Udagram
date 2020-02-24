676b4992ac65b66e47029036ed99f53291aea507
Drew Kimberly <andrew.e.kimberly@gmail.com>
2020-02-24 09:28:19 -0500

    Refactor backend into separate microservice packages (#2)
    * Refactor backend into separate microservice packages
    
    * Make sure split/deploy stages are conditional on master merge
    
    * Update TravisCI script file permissions
    
    * Hoist devDependencies to monorepo loot b/c Lerna and upgrade Angular dep
    
    * More fixes for managing udagram-frontend in monorepo

---
+++ scripts/travisci/build.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# npm install -g travis-ci-cloudfront-invalidation
+npm i
+npm run build:prod
---
+++ scripts/travisci/monorepo-split.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+source "../bin/split.sh"
+
+set +x
+
+# Ensure the build can push to our repos.
+mkdir -p ~/.ssh
+echo -n "${PACKAGE_SPLIT_SSH_KEY}" | base64 --decode > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+eval `ssh-agent -s`
+ssh-add ~/.ssh/id_rsa
+ssh-keyscan -H 'github.com' >> ~/.ssh/known_hosts
+
+# Split monorepo packages.
+split_ci_job "udagram-frontend" "packages/udagram-frontend"
+split_ci_job "udagram-feed-svc" "packages/udagram-feed-svc"
+split_ci_job "udagram-user-svc" "packages/udagram-user-svc"
+split_ci_job "udagram-auth" "packages/udagram-auth"
+
+set -x
