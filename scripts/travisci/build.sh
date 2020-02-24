98188638788037cfb72262a12d7f4f524e549644
Drew Kimberly <akimberl@redhat.com>
2020-02-24 15:46:38 -0500

    Get Monorepo package split CI job working

--- scripts/travisci/build.sh
+++ scripts/travisci/build.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 # npm install -g travis-ci-cloudfront-invalidation
-npm i
 npm run build:prod
--- scripts/travisci/monorepo-split.sh
+++ scripts/travisci/monorepo-split.sh
@@ -1,21 +1,9 @@
 #! /bin/bash
 
-source "../bin/split.sh"
-
-set +x
-
-# Ensure the build can push to our repos.
-mkdir -p ~/.ssh
-echo -n "${PACKAGE_SPLIT_SSH_KEY}" | base64 --decode > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-eval `ssh-agent -s`
-ssh-add ~/.ssh/id_rsa
-ssh-keyscan -H 'github.com' >> ~/.ssh/known_hosts
+source "$(dirname $(dirname $(realpath $0)))/bin/split.sh"
 
 # Split monorepo packages.
-split_ci_job "udagram-frontend" "packages/udagram-frontend"
-split_ci_job "udagram-feed-svc" "packages/udagram-feed-svc"
-split_ci_job "udagram-user-svc" "packages/udagram-user-svc"
-split_ci_job "udagram-auth" "packages/udagram-auth"
-
-set -x
+split_package "udagram-frontend" "packages/udagram-frontend"
+split_package "udagram-feed-svc" "packages/udagram-feed-svc"
+split_package "udagram-user-svc" "packages/udagram-user-svc"
+split_package "udagram-auth" "packages/udagram-auth"
