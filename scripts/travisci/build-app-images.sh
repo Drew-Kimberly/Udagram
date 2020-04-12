6d51c8b6b9a1f151f8b51c17195ac065b07ef24d
Drew Kimberly <akimberl@redhat.com>
2020-04-12 13:38:17 -0400

    feat: Tag built Docker images with the current commit SHA

--- scripts/travisci/build-app-images.sh
+++ scripts/travisci/build-app-images.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-npm run build-images
-docker login -u "${DOCKER_REGISTRY_USER}" -p "${DOCKER_REGISTRY_PASS}"
+npm run build-images ${TRAVIS_COMMIT}
+docker login -u ${DOCKER_REGISTRY_USER} -p ${DOCKER_REGISTRY_PASS}
 npm run push-images
